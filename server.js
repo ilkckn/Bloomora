5b3bdb3e5d74bdf81db9e5239c3b2a5b3f386316
ilkckn <musa.cekcen@dci-student.org>
2024-08-26 19:43:52 +0200

    updated

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use("/api/user", userRouter);
 // });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist"));
 });
 
 
